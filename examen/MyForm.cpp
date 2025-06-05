faf947afd56be5d9aa40256fae9af347f62b5f05
Carlos <carlos.agudo@estudiantat.upc.edu>
2025-06-06 00:28:10 +0200

    Primer commit

---
+++ examen/MyForm.cpp
@@ -0,0 +1,18 @@
+#include "MyForm.h"
+
+MyForm::MyForm (QWidget* parent) : QWidget(parent)
+{
+  ui.setupUi(this);
+    connect(ui.aturaButton, &QPushButton::clicked, [this]() {
+        ui.widget->atura();
+    });
+
+    connect(ui.engegaButton, &QPushButton::clicked, [this]() {
+        ui.widget->engega();
+    });
+
+    // Conexión para el botón de cambiar cámara
+    connect(ui.cameraButton, &QPushButton::clicked, [this]() {
+        ui.widget->setCamera();
+    });
+}
---
+++ examen/examen.pro
@@ -0,0 +1,16 @@
+TEMPLATE    = app
+QT         += opengl
+
+INCLUDEPATH +=  /usr/include/glm
+INCLUDEPATH += ./Model
+
+FORMS += MyForm.ui
+
+HEADERS += MyForm.h MyGLWidget.h
+
+SOURCES += main.cpp  MyForm.cpp ./Model/model.cpp \
+        MyGLWidget.cpp 
+
+DISTFILES += \
+    shaders/basicLlumShader.frag \
+    shaders/basicLlumShader.vert
